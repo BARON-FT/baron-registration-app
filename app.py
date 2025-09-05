7d8f87890b446c8647f34e6113e19e7e8ea3f780
BARON-FT <baron957856@gmail.com>
2025-09-05 15:43:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', '0bjectif_10k')
 
 # --- NOUVELLE CONNEXION À RAILWAY POSTGRESQL ---
 # Remplacez ces valeurs par vos credentials Railway
-DB_HOST = os.environ.get('DB_HOST', 'postgres.railway.internal')
+DB_HOST = os.environ.get('DB_HOST', 'postgresql://postgres:DiTMltdlgRWUcCOqQLvQiNzpVhyrihGi@caboose.proxy.rlwy.net:13497/railway')
 DB_PORT = os.environ.get('DB_PORT', '5432')
 DB_NAME = os.environ.get('DB_NAME', 'railway')
 DB_USER = os.environ.get('DB_USER', 'postgres')
@@ -111,4 +111,5 @@ def logout():
     return redirect(url_for('admin'))
 
 if __name__ == '__main__':
+
     app.run(debug=True)
